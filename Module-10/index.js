007abbd939f2841340ada9617f7fee4630f6c96a
yeasin168703 <mdyeasin168703@gmail.com>
2023-10-06 22:08:32 +0600

    Module-10

---
+++ Module-10/index.js
@@ -0,0 +1,23 @@
+// addition
+var a = 15;
+var b = 5;
+var c = a+b;
+console.log(c)
+
+// substraction
+var a = 28;
+var b = 14;
+var d = a-b;
+console.log(d)
+
+// multiplication
+var a = 12;
+var b = 8;
+var e = a*b;
+console.log(e)
+
+// division
+var a = 98;
+var b = 7;
+var f = a/b;
+console.log(f)
